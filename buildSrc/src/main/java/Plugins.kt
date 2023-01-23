15b1db54a91d04243a73415f9d0f56b493ddd52f
junsuPark <krpjs0508@gmail.com>
2023-01-23 20:46:03 +0900

    Build Plugins

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,10 @@
+object Plugins {
+    object Build {
+        const val ANDROID_APPLICATION = "com.android.application"
+        const val ANDROID_LIBRARY = "com.android.library"
+        const val KOTLIN_ANDROID = "kotlin-android"
+        const val KOTLIN_KAPT = "kotlin-kapt"
+        const val KTLINT = "org.jlleitschuh.gradle.ktlint"
+        const val HILT_ANDROID = "dagger.hilt.android.plugin"
+    }
+}
