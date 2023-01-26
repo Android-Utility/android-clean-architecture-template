c20a5c8a2399e0b0628dbdd6f03e617177b4f86b
junsuPark <krpjs0508@gmail.com>
2023-01-26 17:36:59 +0900

    gradle plugin versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,4 +4,12 @@ object Versions {
     object Java {
         val JAVA_VERSION = JavaVersion.VERSION_1_8
     }
+
+    object Gradle {
+
+        const val GRADLE_ANDROID_VERSION = "7.3.0"
+        const val GRADLE_KOTLIN_VERSION = "1.6.10"
+
+        const val GRADLE_HILT_ANDROID_VERSION = "2.44"
+    }
 }
