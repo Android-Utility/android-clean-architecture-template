1cbef15db0f185c26481649b1da507af9584593d
junsuPark <krpjs0508@gmail.com>
2023-01-27 11:01:11 +0900

    repository

--- domain/src/main/java/app/junsu/domain/repository/user/UserRepository.kt
+++ domain/src/main/java/app/junsu/domain/repository/user/UserRepository.kt
@@ -1,9 +1,8 @@
 package app.junsu.domain.repository.user
 
+import app.junsu.domain.param.user.auth.SignInParam
+
 interface UserRepository {
 
-    suspend fun login(
-        email: String,
-        password: String,
-    )
+    suspend fun signIn(param: SignInParam)
 }
